bcf39d6c958629eddb4cefdaf7d33b02d9091f3c
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-07-11 18:43:32 +0530

    var vs let vs const started

---
+++ Javascript/VarvsLetvsConst.js
@@ -0,0 +1,20 @@
+// VAR vs LET vs CONST
+
+// Redeclartion -> Var can be redeclared , let and const  cannot be
+
+// const a = 10;
+// const a = 99;
+
+// console.log(a);
+
+// Reintialize -->  var and let can be reintialize , const cannot be
+// const a = 100;
+
+// console.log(a);
+
+// Reassign -- var and let can be assign , const cannot be reassign
+
+const a = 10;
+a = 100;
+
+console.log(a);
