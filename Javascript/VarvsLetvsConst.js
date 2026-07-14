9c1341a2e4ec3771023bf7a776898f23f037f4c2
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-07-14 19:01:30 +0530

    Var let vs const

--- Javascript/VarvsLetvsConst.js
+++ Javascript/VarvsLetvsConst.js
@@ -14,7 +14,51 @@
 
 // Reassign -- var and let can be assign , const cannot be reassign
 
-const a = 10;
-a = 100;
+// const a = 10;
+// a = 100;
+
+// console.log(a);
+
+// Hoisting -- variables and functions will get the memory
+// before the actual code execution
+
+// console.log(a);
+// greet();
+// console.log(a);
+// var a = 10;
+// function greet() {
+//   console.log(a);
+//   a = 99;
+//   console.log(a);
+// }
+
+// console.log(a);
+
+// console.log(a);
+// greet();
+// console.log(a);
+// var a = 10;
+// function greet() {
+//   console.log(a);
+//   var a = 99;
+//   console.log(a);
+// }
+
+// console.log(a);
+
+console.log(a);
+one();
+var a = 10;
+function one() {
+    var a = 99;
+    console.log(a);
+    function two() {
+        console.log(a);
+        a = 199;
+    }
+    console.log(a);
+    two();
+    console.log(a);
+}
 
 console.log(a);
