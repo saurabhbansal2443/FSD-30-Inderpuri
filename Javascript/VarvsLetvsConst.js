9c82fa48ed630b16848cae31fa9d8708b0c62bfa
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-07-16 18:59:44 +0530

    let execution

--- Javascript/VarvsLetvsConst.js
+++ Javascript/VarvsLetvsConst.js
@@ -46,19 +46,48 @@
 
 // console.log(a);
 
-console.log(a);
-one();
-var a = 10;
-function one() {
-    var a = 99;
-    console.log(a);
-    function two() {
-        console.log(a);
-        a = 199;
-    }
-    console.log(a);
-    two();
-    console.log(a);
-}
+// console.log(a);
+// one();
+// var a = 10;
+// function one() {
+//     var a = 99;
+//     console.log(a);
+//     function two() {
+//         console.log(a);
+//         a = 199;
+//     }
+//     console.log(a);
+//     two();
+//     console.log(a);
+// }
 
+// console.log(a);
+
+// Scope --
+
+// var is function scope , let and const are block {} scope
+// var let and const are function scope reason
+// a) functions is also a block of code
+// b) function execution context is deleted after the execution of code that's why all the memore it takes also get's deleted
+// let and const are block scope
+
+// function hey() {
+//   let a = 99;
+//   console.log(a);
+// }
+
+// hey();
+// console.log(a);
+
+// Let and const
+
+// console.log(a);
+let a = 10;
 console.log(a);
+abc();
+console.log(a);
+function abc() {
+  console.log(a);
+  a = 999;
+  console.log(a);
+}
