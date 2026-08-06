8f6509bafe94cff64d92529401acdfa7998bb05c
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-08-06 18:15:00 +0530

    Closures

--- Javascript/Closures.js
+++ Javascript/Closures.js
@@ -1,19 +1,45 @@
 // Closures -- Function combined with its lexical scope is closure or a function always
 //             remeber its lexical scope
 
+// function counter() {
+//   let count = 0;
+
+//   function enter() {
+//     count++;
+//     console.log(count);
+//   }
+
+//   function exit() {
+//     count--;
+//     console.log(count);
+//   }
+
+//   let obj = {
+//     enter: enter,
+//     exit: exit,
+//   };
+
+//   return obj;
+// }
+
 function counter() {
   let count = 0;
 
-  function enter() {
-    count++;
-    console.log(count);
-  }
-
-  function exit() {
-    count--;
-    console.log(count);
-  }
+  return {
+    enter: function () {
+      count++;
+      console.log(count);
+    },
+    exit: function exit() {
+      count--;
+      console.log(count);
+    },
+  };
 }
+
+// let obj = counter();
+// let {enter , exit }  = obj ;
+let { enter, exit } = counter();
 
 enter();
 enter();
