5223c06c0424dbdd7f8cb7a3401eb91c3740effe
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-08-08 18:20:32 +0530

    Rest paramter

--- Javascript/SpreadOperatorAndRestParameter.js
+++ Javascript/SpreadOperatorAndRestParameter.js
@@ -7,16 +7,47 @@
 
 // let res = [...arr1, ...arr2];
 
-let obj1 = {
-  name: "AJAY",
-  age: 57,
-};
+// let obj1 = {
+//   name: "AJAY",
+//   age: 57,
+// };
 
-let obj2 = {
-  age: 99,
-  address: "Bhopal",
-};
+// let obj2 = {
+//   age: 99,
+//   address: "Bhopal",
+// };
 
-let res = { ...obj2, ...obj1 };
+// let res = { ...obj2, ...obj1 };
 
-console.log(res);
+// console.log(res);
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// function sum2(a, b, c) {
+//   return a + b + c;
+// }
+
+// Rest Parameters
+
+// function sum(...args) {
+//   let arr = args;
+
+//   return arr.reduce(function (acc, currEle) {
+//     return acc + currEle;
+//   }, 0);
+// }
+
+// console.log(sum(1, 2, 3, 4, 46));
+
+function sum(a, b, ...args) {
+  console.log(a, b);
+  let arr = args;
+
+  return arr.reduce(function (acc, currEle) {
+    return acc + currEle;
+  }, 0);
+}
+
+console.log(sum(1, 2, 3, 4, 46));
