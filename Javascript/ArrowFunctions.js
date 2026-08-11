f37475f807af0250e4ef9a7d5bf166b0aafc8501
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-08-11 18:33:08 +0530

    Add arrow functions and template literals examples

--- Javascript/ArrowFunctions.js
+++ Javascript/ArrowFunctions.js
@@ -1,1 +1,24 @@
-// Arrow 
+// Arrow functions
+// function greet() {
+//   console.log("Hello World");
+// }
+// function sum(a, b) {
+//   return a + b;
+// }
+let greet = () => {
+  console.log("Hello world");
+};
+// greet();
+// let sum = (a, b) => {
+//   return a + b;
+// };
+
+// let sum = (a, b) => a + b;
+
+// console.log(sum(2, 3));
+
+// IIFE -- immediatly invoked function expression
+
+(function () {
+  console.log("kufydfghefd");
+})();
---
+++ Javascript/TemplateLiterals.js
@@ -0,0 +1,9 @@
+// function greet(name, age, message) {
+//   console.log("Hey " + message + " my self " + name + " and my age is " + age);
+// }
+
+function greet(name, age, message) {
+  console.log(`Hey ${message} my self ${name} and my age is ${age}`);
+}
+
+greet("Ajay", 30, "GoodMorning");
