718d713cecc864db359aba901c265a5c37d30eab
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-08-20 17:33:17 +0530

    Pollyfill practise

--- Javascript/PolyfillPractise.js
+++ Javascript/PolyfillPractise.js
@@ -1,24 +1,46 @@
-// Pollyfill --
+// // Pollyfill --
 
-// Map Pollyfill
+// // Map Pollyfill
 
-let arr = [100, 200, 300, 400];
-function cb(ele, index, arr) {
-  return ele * 2;
-}
-// let ans = arr.map(cb);
+// let arr = [100, 200, 300, 400];
+// function cb(ele, index, arr) {
+//   return ele * 2;
+// }
+// // let ans = arr.map(cb);
+// // console.log(ans);
+
+// Array.prototype.customMap = function (cb) {
+//   let arr = this;
+//   let res = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     let cbRes = cb(arr[i], i, arr);
+//     res.push(cbRes);
+//   }
+//   return res;
+// };
+
+// let ans = arr.customMap(cb);
+
 // console.log(ans);
 
-Array.prototype.customMap = function (cb) {
+// Filter --
+
+let arr = [1, 2, 3, 4, 5];
+function cb(ele, index, arr) {
+  return ele % 2 == 0;
+}
+// let ans = arr.filter(cb);
+// console.log(ans);
+Array.prototype.customFilter = function (cb) {
   let arr = this;
   let res = [];
   for (let i = 0; i < arr.length; i++) {
     let cbRes = cb(arr[i], i, arr);
-    res.push(cbRes);
+    if (cbRes == true) {
+      res.push(arr[i]);
+    }
   }
   return res;
 };
-
-let ans = arr.customMap(cb);
-
+let ans = arr.customFilter(cb);
 console.log(ans);
