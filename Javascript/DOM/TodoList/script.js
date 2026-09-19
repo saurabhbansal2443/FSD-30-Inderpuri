fa863392029a8ecd9e514a3039a0139b4c874cb9
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-09-19 17:48:54 +0530

    remove bugs and delete functionality

--- Javascript/DOM/TodoList/script.js
+++ Javascript/DOM/TodoList/script.js
@@ -6,12 +6,18 @@ const taskContainerEle = document.querySelector(".taskContainer");
 // console.log(addBtnEle);
 
 addBtnEle.addEventListener("click", function () {
-  let task = inputEle.value;
+  let task = inputEle.value.trim();
+  inputEle.value = "";
+  if (task.length == 0) {
+    alert("Task Cannot be empty");
+    return;
+  }
   // console.log(task);
   let taskEle = document.createElement("div");
   taskEle.classList.add("task");
-  taskEle.innerHTML = ` <p>${task}</p>
+  taskEle.innerHTML = `<p>${task}</p>
         <svg
+         id="delete"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           width="24"
@@ -22,6 +28,12 @@ addBtnEle.addEventListener("click", function () {
             d="M17 6H22V8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8H2V6H7V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V6ZM9 11V17H11V11H9ZM13 11V17H15V11H13ZM9 4V6H15V4H9Z"
           ></path>
         </svg>`;
+
+  const deleteButton = taskEle.querySelector("#delete");
+
+  deleteButton.addEventListener("click", function () {
+    taskContainerEle.removeChild(taskEle);
+  });
 
   taskContainerEle.appendChild(taskEle);
   //   console.log(taskEle);
